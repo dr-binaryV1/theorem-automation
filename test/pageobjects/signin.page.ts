a251338844884593ba2719618e8fbacdcfd2355b
Damian Wynter <damian@peerledger.com>
2021-09-07 19:24:12 -0300

    create signin page object

---
+++ test/pageobjects/signin.page.ts
@@ -0,0 +1,42 @@
+import Page from './page';
+
+/**
+ * sub page containing specific selectors and methods for a specific page
+ */
+class SigninPage extends Page {
+    /**
+     * define selectors using getter methods
+     */
+    get signinNavButton () { return $('a=Log in') }
+    get signupModalLabel () { return $('h5=Log in') }
+    get nameOfUserLabel () { return $('a#nameofuser') }
+    get logOutNav () { return $('a=Log out') }
+    get inputUsername () { return $('input#loginusername') }
+    get inputPassword () { return $('input#loginpassword') }
+    get btnSubmit () { return $('button=Log in') }
+    get closeBtn () { return $('div.modal:nth-child(3) div.modal-footer button:nth-child(1)') }
+
+    /**
+     * a method to encapsule automation code to interact with the page
+     * e.g. to login using username and password
+     */
+    async signin (username: string, password: string) {
+        await this.signinNavButton.waitForClickable()
+        await this.signinNavButton.click()
+        await this.signupModalLabel.waitForDisplayed()
+        await this.inputUsername.clearValue()
+        await this.inputPassword.clearValue()
+        await this.inputUsername.setValue(username)
+        await this.inputPassword.setValue(password)
+        await this.btnSubmit.click()
+    }
+
+    /**
+     * overwrite specifc options to adapt it to page object
+     */
+    open () {
+        return super.open('#');
+    }
+}
+
+export default new SigninPage();
