2fdfca99780844b1fdfb634b9dff8a2ebe128f7f
Damian Wynter <damian@peerledger.com>
2021-09-07 19:23:33 -0300

    create wdio interfaces for browser and element command

---
+++ wdio.ts
@@ -0,0 +1,9 @@
+declare namespace WebdriverIO {
+    interface Browser {
+        browserCustomCommand: (arg: any) => Promise<void>
+    }
+
+    interface Element {
+        elementCustomCommand: (arg: any) => Promise<number>
+    }
+}
