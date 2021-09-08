6d21c753faca0500f1fc16fb521c4f2869109160
Damian Wynter <developerdamian@gmail.com>
2021-09-08 19:50:58 -0300

    create about spec

---
+++ test/specs/about.ts
@@ -0,0 +1,24 @@
+import AboutusPage from  '../pageobjects/about.page';
+
+describe('About us component', () => {
+    before(async () => {
+        await AboutusPage.open()
+        await AboutusPage.aboutusNavButton.click()
+    })
+
+    it('should be able to click and start, pause and resume video', async () => {
+        await AboutusPage.playAboutusVideo()
+        expect(AboutusPage.player).toHaveElementClass('vjs-playing')
+
+        //Video should be playing so clicking the small pause button on the player should pause video
+        await AboutusPage.pauseAboutusVideo()
+        expect(AboutusPage.player).toHaveElementClass('vjs-paused')
+
+        //Video should be paused so clicking the small play button should resume the player
+        await AboutusPage.resumeAboutusVideo()
+        expect(AboutusPage.player).toHaveElementClass('vjs-playing')
+        
+        await AboutusPage.modalCloseButton.waitForClickable()
+        await AboutusPage.modalCloseButton.click()
+    });
+});
