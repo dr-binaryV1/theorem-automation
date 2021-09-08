3d524f3f72a2fbeff43fae91fb03a50ac581bdfd
Damian Wynter <developerdamian@gmail.com>
2021-09-08 19:45:28 -0300

    rename getters

--- test/pageobjects/signin.page.ts
+++ test/pageobjects/signin.page.ts
@@ -7,14 +7,13 @@ class SigninPage extends Page {
     /**
      * define selectors using getter methods
      */
-    get signinNavButton () { return $('a=Log in') }
     get signupModalLabel () { return $('h5=Log in') }
     get nameOfUserLabel () { return $('a#nameofuser') }
     get logOutNav () { return $('a=Log out') }
     get inputUsername () { return $('input#loginusername') }
     get inputPassword () { return $('input#loginpassword') }
     get btnSubmit () { return $('button=Log in') }
-    get closeBtn () { return $('div.modal:nth-child(3) div.modal-footer button:nth-child(1)') }
+    get modalCloseButton () { return $('div.modal:nth-child(3) div.modal-footer button:nth-child(1)') }
 
     /**
      * a method to encapsule automation code to interact with the page
