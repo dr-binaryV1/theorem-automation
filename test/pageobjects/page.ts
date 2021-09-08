3faf49fab6a68005c9955749db5e5eda0031b947
Damian Wynter <developerdamian@gmail.com>
2021-09-08 19:44:13 -0300

    add global getters to parent page

--- test/pageobjects/page.ts
+++ test/pageobjects/page.ts
@@ -3,6 +3,9 @@
 * that is shared across all page objects
 */
 export default class Page {
+    get signinNavButton () { return $('a=Log in') }
+    get aboutusNavButton () { return $('a=About us') }
+    get signupNavButton () { return $('a#signin2') }
     get next () { return $('button=Next') }
     get previous () { return $('button=Previous') }
 
