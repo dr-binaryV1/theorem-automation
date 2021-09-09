d5a26c12fce75851137ac22ead4692be592d9144
Damian Wynter <developerdamian@gmail.com>
2021-09-09 10:39:11 -0300

    remove hard coded categories

--- test/specs/product.ts
+++ test/specs/product.ts
@@ -1,11 +1,11 @@
 import ProductPage from  '../pageobjects/product.page'
-import { productData } from '../testdata.json'
+import { productData, productCategories } from '../testdata.json'
 
 describe('Product component', () => {
     it('Should correctly search for items on the phone page', async () => {
         await ProductPage.open();
 
-        await ProductPage.selectProductType('Phones')
+        await ProductPage.selectProductType(productCategories.Phone)
         const elemsFound = await $$(`a=${productData.phone.name}`)
         expect(elemsFound.length).toBeGreaterThanOrEqual(1)
     });
@@ -13,7 +13,7 @@ describe('Product component', () => {
     it('Should correctly search for items on the Laptops page', async () => {
         await ProductPage.open();
 
-        await ProductPage.selectProductType('Laptops')
+        await ProductPage.selectProductType(productCategories.Laptop)
         const elemsFound = await $$(`a=${productData.laptop.name}`)
         expect(elemsFound.length).toBeGreaterThanOrEqual(1)
     });
@@ -21,7 +21,7 @@ describe('Product component', () => {
     it('Should correctly search for items on the Monitors page', async () => {
         await ProductPage.open();
 
-        await ProductPage.selectProductType('Monitors')
+        await ProductPage.selectProductType(productCategories.Monitor)
         const elemsFound = await $$(`a=${productData.monitor.name}`)
         expect(elemsFound.length).toBeGreaterThanOrEqual(1)
     });
