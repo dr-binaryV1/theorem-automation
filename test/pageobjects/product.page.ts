0738a388261bb7f2a76b98c088215420b68c7a62
Damian Wynter <developerdamian@gmail.com>
2021-09-08 22:14:16 -0300

    create function to add items to cart

--- test/pageobjects/product.page.ts
+++ test/pageobjects/product.page.ts
@@ -4,17 +4,11 @@ import Page from './page';
  * sub page containing specific selectors and methods for a specific page
  */
 class ProductPage extends Page {
-    /**
-     * define selectors using getter methods
-     */
     get productPhones () { return $('a=Phones') }
     get productLaptops () { return $('a=Laptops') }
     get productMonitors () { return $('a=Monitors') }
-
-    /**
-     * a method to encapsule automation code to interact with the page
-     * e.g. to login using username and password
-     */
+    get addToCartButton () { return $('a=Add to cart') }
+    
     async selectProductType (category: string) {
         // Conditional statement used to determine which category should be clicked before searching for item
         if (category === 'Phones') {
@@ -26,6 +20,23 @@ class ProductPage extends Page {
         }
 
         await browser.pause(2000)
+    }
+
+    async addItemToCart (category: string, productName: string) {
+        await this.selectProductType(category)
+        const elemsFound = await $$(`a=${productName}`)[0]
+        await elemsFound?.click()
+        const elemsTitle = await $$(`h2=${productName}`)[0]
+        await elemsTitle?.waitForDisplayed()
+        await this.addToCartButton.waitForClickable()
+        await this.addToCartButton.click()
+
+        await browser.waitUntil(browser.isAlertOpen)
+        const alertText = await browser.getAlertText()
+
+        expect(alertText).toEqual('Product added')
+        await browser.acceptAlert()
+        await this.productNavButton.click()
     }
     
     open () {
